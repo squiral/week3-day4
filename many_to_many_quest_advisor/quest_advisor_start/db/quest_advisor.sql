cdb82a4ef6254ab60feaae372004dea1abe712d8
Ralph Burden <ralphburden@hotmail.com>
2019-04-18 12:45:46 +0100

    add quest_advisor many to many

--- many_to_many_quest_advisor/quest_advisor_start/db/quest_advisor.sql
+++ many_to_many_quest_advisor/quest_advisor_start/db/quest_advisor.sql
@@ -1,3 +1,4 @@
+DROP TABLE visits;
 DROP TABLE users;
 DROP TABLE locations;
 
@@ -10,4 +11,11 @@ CREATE TABLE locations (
   id SERIAL4 PRIMARY KEY,
   category VARCHAR(255),
   name VARCHAR(255)
+);
+
+CREATE TABLE visits (
+  id SERIAL4 PRIMARY KEY,
+  user_id INT4 REFERENCES users(id) ON DELETE CASCADE,
+  location_id INT4 REFERENCES locations(id) ON DELETE CASCADE,
+  review TEXT
 );
